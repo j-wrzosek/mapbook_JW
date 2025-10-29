679355c9b8dc0e057b89ec2a49dc47eada017409
vequr <u56175@student.wat.edu.pl>
2025-10-29 09:10:20 +0100

    wip: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello World')
+zmienna_1:str= 'Jakub'
+zmienna_2:str= 'Jagoda'
+zmienna_3:str= 'Jagoda'
+zmienna_4:str= 'Jagoda'
+
+uzytkownicy:list=[zmienna_1,zmienna_2,zmienna_3,zmienna_4]
+for uzytkownik in uzytkownicy:
+    print(f'hello {uzytkownik}')
+
