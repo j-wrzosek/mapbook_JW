41f97129254594be819802d04bcb2faefcf95a89
vequr <u56175@student.wat.edu.pl>
2025-11-04 14:10:00 +0100

    chore: refactorized code with envc template

--- main.py
+++ main.py
@@ -1,105 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
+from mapbook_lib.controller import user_info, add_user, remove_user, update_user, get_map
 
-users: list = [
-    {'name': 'Kasia', 'location': 'Warszawa', 'posts': 3,'img_url':''},
-    {'name': 'Asia', 'location': 'Kraków', 'posts': 5,'img_url':''},
-    {'name': 'Basia', 'location': 'Wrocław', 'posts': 7,'img_url':''},
-]
+from mapbook_lib.model import users
 
+def main():
+    while True:
+        print('==========================MENU===============================')
+        print('0. Wyjście')
+        print('1. Wyświetlanie znajomego')
+        print('2. Dodaj znajomego')
+        print('3. Usuń znajomego')
+        print('4. Aktualizuj znajomego')
+        print('5. Generuj mapę')
+        print('=============================================================')
+        tmp_choice: int = int(input('wybierz opcje menu:'))
+        if tmp_choice == 0:
+            break
+        if tmp_choice == 1:
+            print('wybarno funkcje wyswietlania aktywnosci znajomych')
+            user_info(users)
+        if tmp_choice == 2:
+            print('wybrano funkcje dodawania znajomego')
+            add_user(users)
+        if tmp_choice == 3:
+            print('wybrano funkcje usuwania znajomego')
+            remove_user(users)
+        if tmp_choice == 4:
+            print('wybrano funkcje aktualizacji znajomych')
+            update_user(users)
+        if tmp_choice == 5:
+            print('wybrano funkcje wyswietlania mapy')
+            get_map(users)
 
-def user_info(users_data: list) -> None:
-    for user in users_data:
-        print(f'Twoj znajomy {user["name"]} z miejscowosci {user["location"]} opublikowal {user["posts"]} postow.')
-
-
-def add_user(users_data: list) -> None:
-    name: str = input('Podaj imie nowego znajomego: ')
-    location: str = input('Podaj nazwe miejscowosci: ')
-    posts: int = int(input('Podaj liczbe postow: '))
-    img_url: str = input('Wprowadź adres URL zdjęcia: ')
-    users_data.append({'name': name, 'location': location, 'posts': posts, 'img_url': img_url})
-
-
-def remove_user(users_data: list) -> None:
-    tmp_name: str = input('Podaj imie uzytkownika do usuniecia ze znajomych: ')
-    for user in users_data:
-        if user['name'] == tmp_name:
-            users.remove(user)
-
-
-def update_user(users_data: list) -> None:
-    tmp_name: str = input('Podaj imie uzytkownika do aktualizacji: ')
-    for user in users_data:
-        if user['name'] == tmp_name:
-            user['name'] = input('Podaj nowe imie uzytkownika: ')
-            user['location'] = input('Podaj nowa miejscowosc: ')
-            user['posts'] = input('Podaj nowa liczbe postow: ')
-
-
-
-
-
-def get_coordinates(city_name: str) -> list:
-    url: str = f'https://pl.wikipedia.org/wiki/{city_name}'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/120.0.0.0 Safari/537.36'
-    }
-    response = requests.get(url, headers=headers)
-    # print(response.text)
-    response_html = BeautifulSoup(response.text, 'html.parser')
-    # print(response_html.prettify())
-    latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
-    # print(latitude)
-    longitude = float(response_html.select('.longitude')[1].text.replace(',', '.'))
-    # print(longitude)
-    return [latitude, longitude]
-
-
-print(get_coordinates('Wrocław'))
-
-
-def get_map(users_data: list) -> None:
-    import folium
-    m = folium.Map(location=[52.23, 21.0], zoom_start=6)
-
-    for user in users_data:
-        folium.Marker(
-            location=get_coordinates(user['location']),
-            tooltip="Click me!",
-            popup=f'<h4>user: {user['name']}</h4> {user['location']}, {user['posts']}, <img src={user['img_url']} alt="1"/>',
-            icon=folium.Icon(icon="cloud"),
-        ).add_to(m)
-
-    m.save('notatnik.html')
-
-
-while True:
-    print('==========================MENU===============================')
-    print('0. Wyjście')
-    print('1. Wyświetlanie znajomego')
-    print('2. Dodaj znajomego')
-    print('3. Usuń znajomego')
-    print('4. Aktualizuj znajomego')
-    print('5. Generuj mapę')
-    print('=============================================================')
-    tmp_choice: int = int(input('wybierz opcje menu:'))
-    if tmp_choice == 0:
-        break
-    if tmp_choice == 1:
-        print('wybarno funkcje wyswietlania aktywnosci znajomych')
-        user_info(users)
-    if tmp_choice == 2:
-        print('wybrano funkcje dodawania znajomego')
-        add_user(users)
-    if tmp_choice == 3:
-        print('wybrano funkcje usuwania znajomego')
-        remove_user(users)
-    if tmp_choice == 4:
-        print('wybrano funkcje aktualizacji znajomych')
-        update_user(users)
-    if tmp_choice == 5:
-        print('wybrano funkcje wyswietlania mapy')
-        get_map(users)
+if __name__ == '__main__':
+    main()
---
+++ mapbook_lib/model.py
@@ -0,0 +1,5 @@
+users: list = [
+    {'name': 'Kasia', 'location': 'Warszawa', 'posts': 3,'img_url':''},
+    {'name': 'Asia', 'location': 'Kraków', 'posts': 5,'img_url':''},
+    {'name': 'Basia', 'location': 'Wrocław', 'posts': 7,'img_url':''},
+]
