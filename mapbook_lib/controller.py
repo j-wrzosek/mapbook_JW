533651fe9febdc57ab39747548c1fd2d608c42b9
vequr <u56175@student.wat.edu.pl>
2025-11-23 19:23:09 +0100

    WIP: restructured code to MVC

--- mapbook_lib/controller.py
+++ mapbook_lib/controller.py
@@ -1,85 +1,121 @@
-import requests
-from bs4 import BeautifulSoup
+from model import User
 
 
+class Controller:
+    def __init__(self, map_widget, listbox, labels):
+        self.users = []
+        self.markers = []
+        self.map_widget = map_widget
+        self.listbox = listbox
+        self.labels = labels
 
-class User:
-    def __init__(self, name:str, location:str, posts:int, img_url:str):
-        self.name = name
-        self.location = location
-        self.posts = posts
-        self.img_url = img_url
-        self.coords = self.get_coordinates()
+    def add_user(self, name, location, posts, img_url):
+        user = User(name, location, posts, img_url)
+        self.users.append(user)
 
-    def get_coordinates(self):
-        import requests
-        from bs4 import BeautifulSoup
+        marker = self.map_widget.set_marker(user.coords[0], user.coords[1], text=user.name)
+        self.markers.append(marker)
 
-        url: str = f'https://pl.wikipedia.org/wiki/{self.location}'
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                          'AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/120.0.0.0 Safari/537.36'
-        }
-        response = requests.get(url, headers=headers)
-        # print(response.text)
-        response_html = BeautifulSoup(response.text, 'html.parser')
-        # print(response_html.prettify())
-        latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
-        # print(latitude)
-        longitude = float(response_html.select('.longitude')[1].text.replace(',', '.'))
-        # print(longitude)
-        return [latitude, longitude]
+        self.update_listbox()
 
 
+    def delete_user(self):
+        index = self.listbox.index("active")
 
-def user_info(users_data: list) -> None:
-    for user in users_data:
-        print(f'Twoj znajomy {user.name} z miejscowosci {user.location} opublikowal {user.posts} postow.')
+        self.markers[index].delete()
+        self.markers.pop(index)
+
+        self.users.pop(index)
+        self.update_listbox()
+
+    def edit_user(self, name, location, posts, img_url):
+        index = self.listbox.index("active")
+        user = self.users[index]
+
+        user.name = name
+        user.location = location
+        user.posts = posts
+        user.img_url = img_url
+        user.coords = user.get_coordinates()
 
 
-def add_user(users_data: list) -> None:
-    name: str = input('Podaj imie nowego znajomego: ')
-    location: str = input('Podaj nazwe miejscowosci: ')
-    posts: int = int(input('Podaj liczbe postow: '))
-    img_url: str = input('Wprowadź adres URL zdjęcia: ')
-    users_data.append(User(name=name, location=location, posts=posts, img_url=img_url))
-
-
-def remove_user(users_data: list) -> None:
-    tmp_name: str = input('Podaj imie uzytkownika do usuniecia ze znajomych: ')
-    for user in users_data:
-        if user.name == tmp_name:
-            users_data.remove(user)
-
-
-def update_user(users_data: list) -> None:
-    tmp_name: str = input('Podaj imie uzytkownika do aktualizacji: ')
-    for user in users_data:
-        if user.name == tmp_name:
-            user.name = input('Podaj nowe imie uzytkownika: ')
-            user.location = input('Podaj nowa miejscowosc: ')
-            user.posts = input('Podaj nowa liczbe postow: ')
-            user.coords = user.get_coordinates()
+        self . update_listbox
 
 
 
 
-def get_map(users_data: list) -> None:
-    import folium
-    m = folium.Map(location=[52.23, 21.0], zoom_start=6)
 
-    for user in users_data:
-        folium.Marker(
-            location=user.coords,
-            tooltip="Click me!",
-            popup=f'<h4>user: {user.name}</h4> {user.location}, {user.posts}, <img src={user.img_url} alt="1"/>',
-            icon=folium.Icon(icon="cloud"),
-        ).add_to(m)
 
-    m.save('notatnik.html')
 
-if __name__ == '__main__':
-    users_data = []
-    add_user(users_data)
-    remove_user(users_data)
+
+
+# def add_user(users_data: list) -> None:
+#     name: str = entry_name.get()
+#     location: str = entry_lokalizacja.get()
+#     posts:int=int(entry_posty.get())
+#     img_url: str = entry_img_url.get()
+#     users_data.append(User(name=name, location=location, posts=posts, img_url=img_url))
+#     print(users_data)
+#     user_info(users_data)
+#     #czyszczenie formularza
+#     entry_name.delete(0, END)
+#     entry_lokalizacja.delete(0, END)
+#     entry_posty.delete(0, END)
+#     entry_img_url.delete(0, END)
+#     entry_name.focus()
+#
+#
+# def user_info(users_data: list):
+#     listbox_lista_obiektow.delete(0, END)
+#     for idx,user in enumerate(users_data):
+#         listbox_lista_obiektow.insert(idx, f'{user.name} {user.location} {user.posts} posty')
+#
+#
+#
+#
+#
+#
+# def delete_user(users_data: list):
+#     i = listbox_lista_obiektow.index(ACTIVE)
+#     users_data[i].marker.delete()
+#     users_data.pop(i)
+#     user_info(users_data)
+#
+#
+# def user_details(users_data: list):
+#     i = listbox_lista_obiektow.index(ACTIVE)
+#     label_imie_szczegoly_obiektu_wartosc.config(text=users_data[i].name)
+#     label_lokalizacja_szczegoly_obiektu_wartosc.config(text=users_data[i].location)
+#     label_posty__szczegoly_obiektu_wartosc.config(text=users_data[i].posts)
+#     map_widget.set_position(users_data[i].coords[0], users_data[i].coords[1])
+#     map_widget.set_zoom(10)
+#
+#
+# def edit_user(users_data: list):
+#     i = listbox_lista_obiektow.index(ACTIVE)
+#     entry_name.insert(0, users_data[i].name)
+#     entry_lokalizacja.insert(0, users_data[i].location)
+#     entry_posty.insert(0, users_data[i].posts)
+#     entry_img_url.insert(0, users_data[i].img_url)
+#
+#     button_dodaj_obiekt.config(text='Zapisz zmiany', command=lambda: update_user(users_data, i))
+#
+# def update_user(users_data: list, i):
+#     users_data[i].name = entry_name.get()
+#     users_data[i].location = entry_lokalizacja.get()
+#     users_data[i].posts = entry_posty.get()
+#     users_data[i].img_url = entry_img_url.get()
+#
+#     users_data[i].coords=users_data[i].get_coordinates()
+#     users_data[i].marker.set_position(users_data[i].coords[0], users_data[i].coords[1])
+#     users_data[i].marker.set_text(text=users_data[i].name)
+#
+#     user_info(users_data)
+#
+#     button_dodaj_obiekt.config(text="Dodaj obiekt", command=lambda:add_user(users))
+#
+#     entry_name.delete(0, END)
+#     entry_lokalizacja.delete(0, END)
+#     entry_posty.delete(0, END)
+#     entry_img_url.delete(0, END)
+#     entry_name.focus()
--- mapbook_lib/model.py
+++ mapbook_lib/model.py
@@ -1,5 +1,29 @@
-users: list = [
-   # {'name': 'Kasia', 'location': 'Warszawa', 'posts': 3,'img_url':''},
-   # {'name': 'Asia', 'location': 'Kraków', 'posts': 5,'img_url':''},
-   # {'name': 'Basia', 'location': 'Wrocław', 'posts': 7,'img_url':''},
-]
+import requests
+from bs4 import BeautifulSoup
+
+
+class User:
+    def __init__(self, name:str, location:str, posts:int, img_url:str):
+        self.name = name
+        self.location = location
+        self.posts = posts
+        self.img_url = img_url
+        self.coords = self.get_coordinates()
+
+    def get_coordinates(self):
+
+        url: str = f'https://pl.wikipedia.org/wiki/{self.location}'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/120.0.0.0 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
+        # print(response.text)
+        response_html = BeautifulSoup(response.text, 'html.parser')
+        # print(response_html.prettify())
+        latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
+        # print(latitude)
+        longitude = float(response_html.select('.longitude')[1].text.replace(',', '.'))
+        # print(longitude)
+        return [latitude, longitude]
